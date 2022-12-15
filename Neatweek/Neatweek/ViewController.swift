a72d8d09991064b5c4785190b9c949b6290b1e07
mihneabondor <mihnea.bondor@icloud.com>
2022-12-15 16:00:00 +0200

    Initial commit

---
+++ Neatweek/Neatweek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neatweek
+//
+//  Created by Mihnea on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
