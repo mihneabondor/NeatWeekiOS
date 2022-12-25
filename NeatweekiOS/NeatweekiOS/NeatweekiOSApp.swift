3b71a1848a0cdbd230680a469b3683273a95dc4e
mihneabondor <mihnea.bondor@icloud.com>
2022-12-25 14:12:42 +0200

    Reinstated SwiftUI

---
+++ NeatweekiOS/NeatweekiOS/NeatweekiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeatweekiOSApp.swift
+//  NeatweekiOS
+//
+//  Created by Mihnea on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct NeatweekiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
